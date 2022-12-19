7c65cb1bdf1b3829ff0538865718cf7f6a565508
terentyev-va <terentyev.va@yandex.ru>
2022-12-19 07:15:08 +0300

    Track Detail View

--- MyMusic/Search/TrackDetailView/TrackDetailView.swift
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -9,7 +9,38 @@ import UIKit
 
 class TrackDetailView: UIView {
     
-    override class func awakeFromNib() {
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
+    }
+    
+    
 }
